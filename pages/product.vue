cd0ac09154a5972017e24b40bfbecb00a69fa7d6
ralph.hsu <fc.nothung@gmail.com>
2018-10-22 16:54:07 +0800

    商品頁面

---
+++ pages/product.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="container">
+        <div>
+            <h1 class="title">
+                商品名稱
+            </h1>
+            <p class="description">
+                這是商品敘述，絕對激安，錯過可惜
+            </p>
+            <div class="links">
+                <a
+                    href="#"
+                    target="_blank"
+                    class="button--green">加入購物車</a>
+                <a
+                    href="#"
+                    target="_blank"
+                    class="button--grey">立刻購買</a>
+            </div>
+        </div>
+    </section>
+</template>
